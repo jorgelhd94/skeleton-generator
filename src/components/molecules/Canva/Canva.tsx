190c4dba641d8275bea99ae9852e471a12fbf9a1
Jorge Luis Hernandez Dueñas <jorgelhd94@gmail.com>
2023-06-11 11:09:48 -0400

    add transform

--- src/components/molecules/Canva/Canva.tsx
+++ src/components/molecules/Canva/Canva.tsx
@@ -1,18 +1,67 @@
-import { Stage, Layer } from "react-konva";
-import { useState } from "react";
+import { Stage, Layer, Transformer } from "react-konva";
+import { useRef, useState } from "react";
 import CircleElement from "../CanvaElements/CircleElement/CircleElement";
 
+const initialForms = [
+  {
+    id: "rect1",
+    x: 60,
+    y: 60,
+    width: 100,
+    height: 100,
+    fill: "red",
+  },
+  {
+    id: "rect2",
+    x: 150,
+    y: 150,
+    width: 100,
+    height: 100,
+    fill: "green",
+  },
+];
+
 const Canva = () => {
+  const [rectangles, setRectangles] = useState(initialForms);
+  const [selectedId, selectShape] = useState<string | null>(null);
+
+  const checkDeselect = (event: any) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = event.target === event.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
+
   return (
     <>
-      <div className="w-max h-max border-gray-200 border-2 text-neutral-content">
-        <div className="card-body p-1">
-          <Stage width={400} height={400}>
-            <Layer>
-              <CircleElement initialX={100} initialY={100} />
-            </Layer>
-          </Stage>
-        </div>
+      <div className="w-max h-max border-gray-200 bg-gray-200 border-2 text-neutral-content">
+        <Stage
+          width={200}
+          height={200}
+          onMouseDown={checkDeselect}
+          onTouchStart={checkDeselect}
+        >
+          <Layer>
+          {rectangles.map((rect, i) => {
+          return (
+            <CircleElement
+              key={i}
+              shapeProps={rect}
+              isSelected={rect.id === selectedId}
+              onSelect={() => {
+                selectShape(rect.id);
+              }}
+              onChange={(newAttrs: any) => {
+                const rects = rectangles.slice();
+                rects[i] = newAttrs;
+                setRectangles(rects);
+              }}
+            />
+          );
+        })}
+          </Layer>
+        </Stage>
       </div>
     </>
   );
--- src/components/molecules/CanvaElements/CircleElement/CircleElement.tsx
+++ src/components/molecules/CanvaElements/CircleElement/CircleElement.tsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
-import { Circle } from "react-konva";
+import { useState, useRef, useEffect } from "react";
+import { Circle, Transformer } from "react-konva";
 
 interface CircleElementProps {
-  initialX: number;
-  initialY: number;
+  shapeProps: any;
+  isSelected: boolean;
+  onSelect: CallableFunction;
+  onChange: CallableFunction;
 }
 
-const CircleElement = ({ initialX, initialY }: CircleElementProps) => {
+const CircleElement = ({
+  shapeProps,
+  isSelected,
+  onSelect,
+  onChange,
+}: CircleElementProps) => {
+  const shapeRef = useRef<any>();
+  const trRef = useRef<any>();
+
   const handleMouseEnter = (event: any) => {
     event.target.getStage().container().style.cursor = "pointer";
   };
@@ -15,27 +25,66 @@ const CircleElement = ({ initialX, initialY }: CircleElementProps) => {
     event.target.getStage().container().style.cursor = "default";
   };
 
-  const [position, setPosition] = useState({
-    x: initialX,
-    y: initialY,
-  });
+  const onTransformEnd = (event: any) => {
+    const node = shapeRef.current;
+    const scaleX = node.scaleX();
+    const scaleY = node.scaleY();
 
-  const handleDragMove = (event: any) => {
-    const { x, y } = event.target.attrs;
-    setPosition({ x, y });
+    // we will reset it back
+    node.scaleX(1);
+    node.scaleY(1);
+    onChange({
+      ...shapeProps,
+      x: node.x(),
+      y: node.y(),
+      // set minimal value
+      width: Math.max(5, node.width() * scaleX),
+      height: Math.max(node.height() * scaleY),
+    });
   };
 
+  useEffect(() => {
+    if (isSelected && trRef.current) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
   return (
-    <Circle
-      x={position.x}
-      y={position.y}
-      radius={50}
-      fill="gray"
-      draggable
-      onDragMove={handleDragMove}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    />
+    <>
+      <Circle
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        radius={30}
+        fill="gray"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onTransformEnd={onTransformEnd}
+      />
+
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          rotateEnabled={false}
+          borderDash={[3, 3]}
+          anchorFill="#4B6BFB"
+          borderStroke="#4B6BFB"
+          anchorStroke="#4B6BFB"
+          anchorSize={5}
+        />
+      )}
+    </>
   );
 };
 
